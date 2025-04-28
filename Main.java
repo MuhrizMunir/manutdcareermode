1d949bebd95fd2eaed13e2ea08b6bee4448fc40f
MuhrizMunir <mm7063@pleasantonusd.net>
2025-04-28 21:10:09 +0000

    finsihed loading screen, etc

--- Main.java
+++ Main.java
@@ -1,6 +1,4 @@
 import java.util.Scanner;
-import java.util.concurrent.TimeUnit;
-
  public class Main {
 	//clear screen method: 
 		//System.out.print("\033[H\033[2J");
@@ -12,45 +10,93 @@ import java.util.concurrent.TimeUnit;
 		String ball = "⚽";  
 		String save = "🧤 "; 
 		String miss = "❌"; 
-		String goal = "✅"; 
+		String goal = "✅";
+		boolean condition = true; 
+		String action = "nothing";
 		//empty screen
+		// System.out.print("\033[H\033[2J");
+    	// System.out.flush();
+		
+		//loading screen
+		String load = "\t\t\t\tLoading . . .";
+		String load1 = "\t\t\t\tInitializing all game features . . .";
+		String load2 = "\t\t\t\tCreating teams . . .";
+		String load3 = "\t\t\t\tMowing the pitch . . .";
+		String load4 = "\t\t\t\tWarming up the bench . . .";
+		String load5 = "\t\t\t\tPlayers are stretching . . .";
 		System.out.print("\033[H\033[2J");
     	System.out.flush();
-		//loading screen, work on later if possible? display random msgs with delay
-		// for(int i=0; i<10; i++){
-		// 	String load = "loading . . .";
-		// 	System.out.println(load);
-		// 	System.out.print("\033[H\033[2J");
-    	// 	System.out.flush();
-		// }
+		System.out.println(load);
+		try {
+			Thread.sleep(1000);
+		} catch(InterruptedException e) {
+		System.out.println("error");
+		}
+		System.out.print("\033[H\033[2J");
+    	System.out.flush();
+		System.out.println(load1);
+		try {
+			Thread.sleep(1000);
+		} catch(InterruptedException e) {
+		System.out.println("error");
+		}
+		System.out.print("\033[H\033[2J");
+    	System.out.flush();
+		System.out.println(load2);
+		try {
+			Thread.sleep(1000);
+		} catch(InterruptedException e) {
+		System.out.println("error");
+		}
+		System.out.print("\033[H\033[2J");
+    	System.out.flush();
+		System.out.println(load3);
+		try {
+			Thread.sleep(1000);
+		} catch(InterruptedException e) {
+		System.out.println("error");
+		}
+		System.out.print("\033[H\033[2J");
+    	System.out.flush();
+		System.out.println(load4);
+		try {
+			Thread.sleep(1000);
+		} catch(InterruptedException e) {
+		System.out.println("error");
+		}
+		System.out.print("\033[H\033[2J");
+    	System.out.flush();
+		System.out.println(load5);
+		try {
+			Thread.sleep(1000);
+		} catch(InterruptedException e) {
+		System.out.println("error");
+		}
+		System.out.print("\033[H\033[2J");
+    	System.out.flush();
 		//end of loading screen
-
+		
+		//game intro
+		while(!action.equalsIgnoreCase("exit")){ 
 		System.out.println("Welcome to the game, young manager! What is your name?");
 		String managerName = scanner.nextLine(); 
 		System.out.print("\033[H\033[2J");
     	System.out.flush();
 		System.out.println(managerName+" you will be managing the greatest team ever, Manchester United! \nBe prepared to sign and fire players, motivate your team to victory, and take actions in the game to secure us the ultimate trophy!");
 		System.out.println("What actions do you want to take? Input the number corresponding to your selection.");
-		System.out.println("1. \n2.\n3.\n4.\n5.\n6.\n7.");
-		String action = scanner.nextLine();
-
+		System.out.println("1. Play the final\n2. Check your lineup\n3.\n4.\n5.\n6.\n7.Quit game");
+		action = scanner.nextLine().trim();
 		
-		//uncomment later lines 33-40 and 45
-		int count = 1;  
-		TestPlayer[] team1 = new TestPlayer[6];  
-			for(int i=0;i<team1.length-1;i++){  
-				String str = count + ""; 
-				team1[i] = new TestPlayer(str); 
-				count++;
-			} 
-			TestPlayer gkpr = new TestGoalkeeper("6");  
-		// //dont uncomment lines 42-44 and 46
-		// // Game mygame = new Game();    
-		// // Game.showGoal();  
-		// // String gkr1 = " \\ o / \n   |  \n  / \\ ";
-		Tools.playGame(scanner);  
-		// //System.out.println(ball + save + miss + goal);
 		
+		
+		
+		Game mygame = new Game();    
+		Game.showGoal();  
+		String gkr1 = " \\ o / \n   |  \n  / \\ ";
+		Tools.playGame(scanner);  	
+		System.out.println(ball + save + miss + goal);
+		}
+		System.out.println("Goodbye!");
 		
 	} 
 }
